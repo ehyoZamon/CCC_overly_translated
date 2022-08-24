87d63bdc3570cbc08d6f6005a78d315fb9f7b5aa
Mirzobakhtiyor Rahmatov <ehyoZamon@gmail.com>
2022-08-24 13:19:51 +0200

    Train station was added

--- Cynteract-games/Assets/Games/Platformer/Scripts/Moving Platform/Train_engine.cs
+++ Cynteract-games/Assets/Games/Platformer/Scripts/Moving Platform/Train_engine.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
 namespace Cynteract.Platformer
 {
     public class Train_engine: MonoBehaviour
@@ -8,6 +10,8 @@ namespace Cynteract.Platformer
         public MovingPlatform platform;
         public Transform start, end;
         public float train_stop_duration=3;
+        public GameObject[] wheels;
+        public TMP_Text[] time_terminals;
         private float train_stop_timer=0;
         float startTime;
         public float speed = 1;
@@ -29,18 +33,28 @@ namespace Cynteract.Platformer
 
         private void FixedUpdate()
         {
+            for(int k=0;k<time_terminals.Length;k++){
+                time_terminals[k].text=System.DateTime.Now+"";
+            }
+
+    
+            float t = Time.fixedTime - startTime;
+            float velocityLength = Mathf.Cos(t * speed) * 0.5f * speed;
+            platform.velocity = (end.position - start.position) * velocityLength;
+            
             if(trainStoped){
                 train_stop_timer-=0.01f;
                 if(train_stop_timer<0){
-                    trainStoped=false;
+                    if(Math.Abs(platform.transform.position.x-Vector3.Lerp(start.position, end.position, (Mathf.Sin(t * speed) + 1) * 0.5f).x)<0.2){
+                        trainStoped=false;
+                    }
                 }
             }else{
-                float t = Time.fixedTime - startTime;
-                float velocityLength = Mathf.Cos(t * speed) * 0.5f * speed;
-                platform.velocity = (end.position - start.position) * velocityLength;
                 platform.transform.position = Vector3.Lerp(start.position, end.position, (Mathf.Sin(t * speed) + 1) * 0.5f);
-
                 if(backwards){
+                    for(int i=0;i<wheels.Length;i++){
+                        wheels[i].GetComponent<Transform>().Rotate(0,0,-velocityLength*100,Space.Self);
+                    }
                     if(platform.transform.position.x>last_position){
                         //trainStoped=true;
                         train_stop_timer=train_stop_duration;
@@ -48,6 +62,9 @@ namespace Cynteract.Platformer
                         forwards=true;
                     }
                 }else if(forwards){
+                    for(int i=0;i<wheels.Length;i++){
+                        wheels[i].GetComponent<Transform>().Rotate(0,0,velocityLength*100,Space.Self);
+                    }
                     if(platform.transform.position.x<last_position){
                         //trainStoped=true;
                         train_stop_timer=train_stop_duration;
@@ -55,6 +72,7 @@ namespace Cynteract.Platformer
                         forwards=false; 
                     }
                 }
+
                 last_position=platform.transform.position.x;   
             }
         }
